c4cac2894733b9294bb45f792c9973f4d775dbc3
char3210 <45241413+char3210@users.noreply.github.com>
2023-12-03 20:49:22 -0600

    3 hours of progress (i added a keybind)

---
+++ src/main/java/me/char321/eyezoommod/Eyezoommod.java
@@ -0,0 +1,22 @@
+package me.char321.eyezoommod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+public class Eyezoommod implements ModInitializer {
+    public static KeyBinding toggleKey;
+
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.eyezoommod.zoom",
+                InputUtil.UNKNOWN_KEY.getCode(),
+                "Eye Zoom"
+        ));
+    }
+}
